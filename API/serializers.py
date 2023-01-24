e80f487149e721c4648f8678145d14c3d99ff7cb
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-01-24 06:03:37 -0600

    Modificando serializador antes de validaciones

--- API/serializers.py
+++ API/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers 
 from .models import Category, MenuItem
+from decimal import Decimal
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +8,14 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    stock = serializers.IntegerField(source='inventory')
+    price_after_tax = serializers.SerializerMethodField(method_nam='calculate_tax')
+    category_id = serializers.IntegerField(write_only=True)
+    category = CategorySerializer(read_only=True)
+
     class Meta:
         model = MenuItem
-        fields = '__all__'
+        fields = ['id','title','price','stock', 'price_after_tax','category','category_id']
+
+    def calculate_tax(self, product:MenuItem):
+        return product.price * Decimal(1.1)
