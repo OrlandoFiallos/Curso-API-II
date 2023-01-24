8bfd73a0c29dc2fdd5cde50bec5c68c349adea05
OrlandoFiallos <contactofiallosorlando@gmail.com>
2023-01-24 05:39:57 -0600

    Añadiendo modelo Watches

--- API/models.py
+++ API/models.py
@@ -19,3 +19,22 @@ class MenuItem(models.Model):
     
     def __str__(self):
         return self.title
+
+class WatchesCategory(models.Model):
+    slug = models.SlugField(editable=False)
+    title = models.CharField(max_length=255) 
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+    def __str__(self):
+        return self.title
+
+class Watches(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
+    
+    def __str__(self):
+        return self.title
